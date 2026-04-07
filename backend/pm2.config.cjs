d27712032be0dbf994dd3867e989abba1a45efdb
wildananugrah <wildananugrah@gmail.com>
2026-04-07 14:49:59 +0700

    adjust script

---
+++ backend/pm2.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [{
+    name: "fce-backend",
+    script: "src/index.ts",
+    interpreter: "bun",
+    exec_mode: "fork",
+    env: {
+      NODE_ENV: "development",
+    }
+  }],
+};
