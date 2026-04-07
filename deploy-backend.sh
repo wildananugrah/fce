dc0fdda8a722a0ec7499185a20c8f4edad0311a0
root <root@ip-172-26-13-198.ap-southeast-1.compute.internal>
2026-04-07 08:19:17 +0000

    update

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -4,11 +4,18 @@ set -e
 REPO_DIR="/root/repo/fce"
 DEPLOY_DIR="/var/www/html/fce"
 
+echo "Restart Database, Minio, Monitoring"
+cd "$REPO_DIR"
+make down; make up
+
 echo "Building database..."
 cd "$REPO_DIR/backend"
+set -a
+source .env
+set +a
 bun install
-bunx prisma generate
 bunx prisma db push
+bunx prisma generate
 
 echo "Building backend..."
 cd "$REPO_DIR/backend"
